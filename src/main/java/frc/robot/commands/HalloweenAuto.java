ce3262c4a9862005ebfbe7e079eee23c22af79fb
Cadin Cross <rocket48875@gmail.com>
2021-10-25 17:55:34 -0400

    Halloween Changes 1.0

---
+++ src/main/java/frc/robot/commands/HalloweenAuto.java
@@ -0,0 +1,52 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+
+public class HalloweenAuto extends CommandBase {
+    private final Drivetrain drivetrain;
+    AnalogInput distanceSensor = new AnalogInput(0);
+    private double voltage;
+    private int range;
+
+
+    public HalloweenAuto(Drivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+        addRequirements(drivetrain);
+
+    }
+
+    public int Convert(double voltage) {
+        double vi = 5/1024;
+        double rangeMm = 5.0*(voltage/vi);
+        double rangeM = rangeMm/1000;
+        return (int)rangeM;
+    }
+
+    @Override
+    public void initialize() {
+        
+    }
+
+    @Override
+    public void execute() {
+        this.voltage = distanceSensor.getVoltage();
+        this.range = Convert(this.voltage);
+        System.out.println(this.range);
+        if(this.range < 1.5){
+            
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (false);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.stop();
+    }
+}
